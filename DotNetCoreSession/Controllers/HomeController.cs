c207c3509254ff561a4e1ef9b45ef6cea25aebbe
Hamza Taşhamza <hamza.tas@proj-e.com>
2021-01-17 12:10:49 +0300

    DotNetCoreSessionProject

---
+++ DotNetCoreSession/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using DotnetCoreSession.Models;
+using DotnetCoreSession.Helpers;
+
+namespace DotnetCoreSession.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            HttpContext.Session.SetString("sessionKeyStr", "hamzatas");
+            HttpContext.Session.SetInt32("sessionKeyInt", 100);
+
+            ViewBag.sessionKeyStr = HttpContext.Session.GetString("sessionKeyStr");
+            ViewBag.sessionKeyInt = HttpContext.Session.GetInt32("sessionKeyInt");
+
+
+            // Store Complex data in session
+
+           List<User> _users = new List<User>()
+           {
+               new User() 
+               {
+                   Firstname = "Hamza",
+                   Lastname = "TAŞ",
+                   Mail = "hamzatas@*****.com",
+                   Phone = "05*********",                   
+               }
+           };
+
+            SessionHelper.SetObjectAsJson(HttpContext.Session,"User", _users);
+
+            List<User> _sessionList = SessionHelper.GetObjectFromJson<List<User>>(HttpContext.Session, "User"); ;
+
+            ViewBag.UserList = _sessionList;
+
+
+            return View();
+        }
+    }
+}
---
+++ DotNetCoreSession/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotnetCoreSession.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Mail { get; set; }
+        public string Phone { get; set; }
+    }
+}
